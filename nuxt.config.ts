85ecf8d9e0e11f05bd65ad609a2f4bc59573d0a9
a-diakite <aboubakarsiriki60@gmail.com>
2026-05-15 01:30:29 +0200

    chore: enable typescript strict + eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  compatibilityDate: '2026-05-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  typescript: {
+    strict: true,
+    typeCheck: false,
+  },
+  ssr: false,
+  app: {
+    head: {
+      title: 'dony PRO',
+      htmlAttrs: { lang: 'fr', class: 'dark' },
+    },
+  },
 })
